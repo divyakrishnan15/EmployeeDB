7a67e2e72f6f023a93265b600380e3c4484989e1
divyakrishnan15 <divyabalajee@gmail.com>
2023-07-11 07:10:43 -0400

    update query

--- dbCommands.js
+++ dbCommands.js
@@ -120,6 +120,123 @@ function dbQueries(res) {
           });
       });
       break;
+      case "Update an Employee Role":
+        db.query("SELECT * FROM employee", (err, employees) => {
+          if (err) {
+            console.log(err);
+          }
+  
+          employees.map((e, i) => {
+            e["name"] = e.first_name + " " + e.last_name;
+            e["value"] = e.emp_id;
+          });
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                message: "What is the Employee's Name?",
+                name: "updEmp_Id",
+                choices: employees,
+              },
+            ])
+            .then((updateRoleName) => {
+              db.query("SELECT * FROM role", (err, roles) => {
+                if (err) {
+                  console.log(err);
+                }
+                roles.map((e, i) => {
+                  e["name"] = e.title;
+                  e["value"] = e.role_id;
+                });
+  
+                inquirer
+                  .prompt([
+                    {
+                      type: "list",
+                      message: "What is the Employee's Role?",
+                      name: "updEmp_Role",
+                      choices: roles,
+                    },
+                  ])
+                  .then((updateRole) => {
+                    let updateAnEmployeeRoleList = [
+                      updateRole.updEmp_Role,
+                      updateRoleName.updEmp_Id,
+                    ];
+                    updateEmployeeRole(updateAnEmployeeRoleList);
+                    nextQuestions();
+                  });
+              });
+            });
+        });
+        break;
+      case "Update Employee Managers":
+        db.query("SELECT * FROM employee", (err, employees) => {
+          if (err) {
+            console.log(err);
+          }
+  
+          employees.map((e, i) => {
+            e["name"] = e.first_name + " " + e.last_name;
+            e["value"] = e.emp_id;
+          });
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                message: "What is the Employee's Name?",
+                name: "updEmp_Id",
+                choices: employees,
+              },
+              {
+                type: "list",
+                message: "What is the Employee's Manager?",
+                name: "updEmpMgr_Id",
+                choices: employees,
+              },
+            ])
+            .then((updateMgr_values) => {
+              updateEmployeeMgr(updateMgr_values);
+              nextQuestions();
+            });
+        });
+        break;
+  
+      case "View Employees by Manager":
+        db.query(
+          `SELECT e1.*, REPLACE(CONCAT_WS(' ',e2.first_name, e2.last_name),'  ',' ') AS 'manager_name'
+        FROM employee e1
+        JOIN employee e2 
+        ON e1.manager_id = e2.emp_id
+        ORDER BY e1.manager_id ASC;`,
+          (err, employees) => {
+            if (err) {
+              console.log(err);
+            }
+            console.table(employees);
+            nextQuestions();
+          }
+        );
+        break;
+  
+      case "View Employees by Department":
+        db.query(
+          `SELECT e.*, r.*, d.name as "department_name"
+            FROM employee e
+            JOIN role r ON r.role_id = e.role_id
+            JOIN department d ON d.dept_id = r.department_id
+            ORDER BY department_id ASC;`,
+          (err, employees) => {
+            if (err) {
+              console.log(err);
+            }
+  
+            console.table(employees);
+            nextQuestions();
+          }
+        );
+        break;
+  
     
 function nextQuestions() {
   // console.log("Next Questions---")
